916fc5fe74d2f2b1a015c27274bf3960bcfc37ae
bramjanssens <bram.janssens@infosupport.com>
2021-01-27 12:02:22 +0100

    First commit: full stack no db.

---
+++ src/jsMain/kotlin/App.kt
@@ -0,0 +1,51 @@
+import kotlinext.js.jsObject
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.launch
+import kotlinx.html.js.onClickFunction
+import react.*
+import react.dom.h1
+import react.dom.li
+import react.dom.ul
+
+private val scope = MainScope()
+
+val App = functionalComponent<RProps> {
+    val (shoppingList, setShoppingList) = useState(emptyList<ShoppingListItem>())
+
+    useEffect(dependencies = listOf()) {
+        scope.launch {
+            setShoppingList(getShoppingList())
+        }
+    }
+
+    h1 {
+        +"Full-Stack Shopping List"
+    }
+    ul {
+        shoppingList.sortedByDescending(ShoppingListItem::priority).forEach { item ->
+            li {
+                key = item.toString()
+                attrs.onClickFunction = {
+                    scope.launch {
+                        deleteShoppingListItem(item)
+                        setShoppingList(getShoppingList())
+                    }
+                }
+                +"[${item.priority}] ${item.desc} "
+            }
+        }
+    }
+
+    child(
+        InputComponent,
+        props = jsObject {
+            onSubmit = { input ->
+                val cartItem = ShoppingListItem(input.replace("!", ""), input.count { it == '!' })
+                scope.launch {
+                    addShoppingListItem(cartItem)
+                    setShoppingList(getShoppingList())
+                }
+            }
+        }
+    )
+}
